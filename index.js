8694fd3583cbe7de487e3369a28f23456aad7782
Henry Chimezie <henry1722chi@gmail.com>
2025-06-05 11:55:04 +0100

    add all files

---
+++ index.js
@@ -0,0 +1,166 @@
+// console.log(`Hello`);
+// console.log(`I like pizza!`);
+// window.alert(`This is an alert`)
+// window.alert(`I like pizza`)
+
+
+// document.getElementById("myH1").textContent = 'Hello'
+// document.getElementById("myP").textContent = 'I like pizza'
+// This is a comment
+
+/*
+This is a comment
+*/
+
+// let x = 123;
+// console.log(x)
+
+// let age = 25;
+// console.log(age)
+
+// let price = 10.99;
+// console.log(price)
+
+// let gpa = 2.1;
+// console.log(gpa)
+
+// console.log(`You are ${age} old`)
+// console.log(`The price is $${price}`)
+// console.log(`Your GPA is ${gpa}`)
+// console.log(typeof gpa)
+
+// let firstName = "bro"
+// console.log(typeof firstName)
+// console.log(`Your name is ${firstName}`)
+// //batik is used for adding/calling variables in a string
+
+// favoriteFood = "Rice"
+// console.log(`You like ${favoriteFood}`)
+
+// let email = "henry1722chi@gmail.com"
+// console.log(`My email is ${email}`)
+
+// let online = true
+// let forSale = false
+// let isStudent = true
+
+// console.log(typeof online)
+// console.log(`Bro is online: ${online}`)
+// console.log(`Is this car for sale: ${forSale}`)
+// console.log(`Enrolled: ${isStudent}`)
+
+// let fullName = "Henry Chimezie"
+// let age = 19
+// let isStudent = true
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`
+// document.getElementById("p2").textContent = `You are ${age} old`
+// document.getElementById("p3").textContent = `Enrolled in school: ${isStudent}`
+
+
+
+
+// let students = 30
+// students = students + 1
+// students = students - 1
+// students = students * 2
+// students = students / 2
+// students = students ** 2// exponent operator
+// let extrastudents = students % 2 //Modulus operator
+
+// students += 2
+// students -= 2
+// students *=2
+// students /=2
+// students %=2
+
+// students++
+// students--
+
+// console.log(students)
+
+// let result = 1 + 2 * 3 + 4 **2
+// let results = 12 % 5 + 8 / 2
+// let results = 6 / 2 ** (2 + 5)
+
+// console.log(results)  
+
+
+
+
+// username = window.prompt("What is your username?")
+// console.log(username) // user input
+// let username;
+// document.getElementById("MySubmit").onclick = function() {
+//     username = document.getElementById("MyText").value;
+//     document.getElementById("text").textContent = `Hello ${username}`;
+// }  // user input using html textbox
+
+
+
+
+// type conversion = change the datatype of a value to another datatype
+
+// let age = window.prompt("How old are you?");
+// age = Number(age); // convert string to number
+// age += 1;
+// console.log(age, typeof age); // check the type of age
+
+// let x;
+// let y;
+// let z;
+
+// x = Number(x); // convert string to number
+// y = String(y);
+// z =  Boolean(z); // convert string to boolean
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+
+
+// Constant
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter the radius of the circle:");
+
+// console.log(circumference);
+// document.getElementById("myH3").textContent = circumference;
+// document.getElementById("MySubmit").onclick = function(){
+//     radius = document.getElementById("MyText").value
+//     radius = Number(radius); // convert string to number
+//     circumference = 2 * PI * radius;
+//     document.getElementById("myH3").textContent = `Radius: ${circumference}cm`
+// }
+
+
+
+// Counter program
+
+// const decreasebtn = document.getElementById("decreasebtn");
+// const resetbtn = document.getElementById("resetbtn");
+// const increasebtn = document.getElementById("increasebtn");
+// const countLabel = document.getElementById("countLabel");
+// let count = 0;
+
+// increasebtn.onclick = function(){
+//     count++
+//     countLabel.textContent = count
+// }
+
+// decreasebtn.onclick = function(){
+//     count--
+//     countLabel.textContent = count
+// }
+
+// resetbtn.onclick = function(){
+//     count = 0;
+//     countLabel.textContent = count;
+// }
+
+
+
